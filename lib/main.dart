844bec37ae924f414a691dff99992c3ef4115300
FlutLab.io <>
2021-05-21 01:04:31 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -74,11 +74,11 @@ class _IngresoSistemaState extends State<IngresoSistema> {
       ), //fin de appbar
       body: SafeArea(
         child: ListView(padding: EdgeInsets.symmetric(horizontal: 24.0), children: <Widget>[
-          SizedBox(height: 80.0),
+          SizedBox(height: 40.0),
           Column(
             children: <Widget>[
               Image.asset('assets/yo.jpg'),
-              SizedBox(height: 1.0),
+              SizedBox(height: 20.0),
             ],
           ), //fin de column
           if (!acceso)
